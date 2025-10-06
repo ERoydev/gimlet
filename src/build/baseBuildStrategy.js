0a53186b2d7e722867e1514a2b78a0221e7cc401
ERoydev <e.roydev@gmail.com>
2025-10-06 19:01:37 +0300

    ref: Remove util

--- src/build/baseBuildStrategy.js
+++ src/build/baseBuildStrategy.js
@@ -1,5 +1,3 @@
-const findExecutableFile = require('../utils/findExecutableFile');
-const debuggerSession = require('../state');
 
 class BaseBuildStrategy {
     constructor(workspaceFolder, packageName, depsPath) {
@@ -8,13 +6,13 @@ class BaseBuildStrategy {
         this.depsPath = depsPath;
     }
 
-    async build(progress) {
+    async build() {
         throw new Error('build() must be implemented by subclasses');
     }
 
     // Base method that all build strategies are going to use in order to find the executables inside target/deploy 
     findDebugExecutable(files) {
-        const executableFile = findExecutableFile(
+        const executableFile = this.findExecutableFile(
             files,
             this.packageName,
             '.debug'
@@ -23,16 +21,24 @@ class BaseBuildStrategy {
         console.log(`Executable path: ${executablePath}`);
         console.log(`Executable file: ${executableFile}`);
 
-        const bpfCompiledFile = findExecutableFile(
+        const bpfCompiledFile = this.findExecutableFile(
             files,
             this.packageName,
             '.so'
         );
         const bpfCompiledPath = `${this.depsPath}/${bpfCompiledFile}`;
-        debuggerSession.globalBpfCompiledPath = bpfCompiledPath;
-        console.log(`BPF compiled path: ${bpfCompiledPath}`);
 
         return { executablePath, bpfCompiledPath };
+    }
+
+    // util for finding the executable file in the target/deploy directory
+    findExecutableFile(files, projectName, extension) {
+        const transformedProjectName = projectName.replace(/-/g, '_');
+        return files.find(
+            (file) =>
+                file.startsWith(`${transformedProjectName}`) &&
+                file.endsWith(extension)
+        );
     }
 }
 
